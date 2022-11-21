dc83edcf87a40b30bde2ac91a151964c3b718394
danijel <danijel.kecman@gmail.com>
2022-11-21 02:19:06 +0100

    - add note view

--- app/Notes/Notes/Home.swift
+++ app/Notes/Notes/Home.swift
@@ -8,19 +8,55 @@
 import SwiftUI
 
 struct Home: View {
+  @State var notes = [Note]()
+  @State var showAddNote = false
+  
   var body: some View {
     NavigationView {
-      List(0..<9) { index in
-        Text("we are at \(index)")
+      List(self.notes) { note in
+        Text(note.note)
           .padding()
       }
+      .sheet(isPresented: $showAddNote, content: {
+        AddNoteView()
+      })
+      .onAppear(perform: {
+        fetchNotes()
+      })
       .navigationTitle("Notes")
       .navigationBarItems(trailing: Button(action: {
-        print("Add a note")
+        self.showAddNote.toggle()
       }, label: {
         Text("Add")
       }))
     }
+  }
+  
+  func fetchNotes() {
+    guard let url = URL(string: "http://localhost:3000/notes") else { return }
+    
+    let task = URLSession.shared.dataTask(with: url) { data, res, err in
+      guard let data else { return }
+      
+      do {
+        let notes = try JSONDecoder().decode([Note].self, from: data)
+        self.notes = notes
+      } catch {
+        print(error)
+      }
+    }
+    task.resume()
+  }
+}
+
+struct Note: Identifiable, Codable {
+  var id: String { noteId }
+  var noteId: String
+  var note: String
+  
+  enum CodingKeys: String, CodingKey {
+    case noteId = "_id"
+    case note
   }
 }
 
